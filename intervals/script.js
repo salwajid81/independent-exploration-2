b678364c277f2bf4a4b98b3868256fbe969ac491
salwajid81 <salwajid81@gmail.com>
2024-08-14 11:28:28 -0400

    Working Counter

--- intervals/script.js
+++ intervals/script.js
@@ -3,3 +3,10 @@ let time = 0;
 // reference to the <span> containing the number
 let counterElement = document.getElementById("time");
 
+//Function updates the counterElement Span Tag so that it can be linked to the time counter in JS.
+function updateTime(){
+    counterElement.innerHTML = time;
+    time++
+}
+// setInterval(the function , time in ms) I used 1000 since it is 1 second
+setInterval(updateTime,1000);
